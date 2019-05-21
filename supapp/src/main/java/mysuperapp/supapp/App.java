6bd89e7aa968da24c0bc1d27625ce6c668fba5e3
Cmode11 <cathalmurray185@outlook.com>
2019-05-21 09:17:40 +0100

    commit suppapp

---
+++ supapp/src/main/java/mysuperapp/supapp/App.java
@@ -0,0 +1,13 @@
+package mysuperapp.supapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
